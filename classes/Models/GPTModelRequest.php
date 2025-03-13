ddce77f2e8ba8eaa361543bdbee545d135aa3527
Jordan Schultz <jmschult@stanford.edu>
2025-03-13 14:27:17 -0700

    Added new functions to alter parameters for new and old GPT model structure

--- classes/Models/GPTModelRequest.php
+++ classes/Models/GPTModelRequest.php
@@ -14,8 +14,12 @@ class GPTModelRequest extends BaseModelRequest {
      */
     public function sendRequest(string $apiEndpoint, array $params): array
     {
-        $apiEndpoint = $this->appendAuthKey($apiEndpoint);
-        $requestData = $this->prepareRequestData($params);
+        if($this->model === "gpt-4o" || $this->model === "ada-002") { //Old model types have different configuration parameters
+            $requestData = $this->prepareOldRequestData($params);
+            $apiEndpoint = $this->appendAuthKey($apiEndpoint);
+        } else { // o1 , Mini
+            $requestData = $this->prepareNewRequestData($params);
+        }
 
         return $this->executeApiCall($apiEndpoint, $requestData);
     }
@@ -38,12 +42,34 @@ class GPTModelRequest extends BaseModelRequest {
      * @param array $params The request parameters.
      * @return string JSON-encoded request data.
      */
-    private function prepareRequestData(array $params): string
+    private function prepareOldRequestData(array $params): string
     {
         $mergedParams = array_merge($this->defaultParams, $params);
 
         // Manually removing default param, breaks regular gpt calls
         unset($mergedParams["reasoning_effort"]);
         return json_encode($mergedParams) ?: '[]';
+    }
+
+    /**
+     * Sets new parameters based on new API spec o1 , o3-mini
+     * @param array $params The request parameters.
+     * @return string JSON-encoded request data.
+     */
+    private function prepareNewRequestData(array $params): string
+    {
+        // Grab required embedded strings
+        $auth_key_name = $this->auth_key_name;
+        $api_key = $this->apiKey;
+
+        // Set headers explicit to llama / o1 / 03-mini
+        $this->setHeaders(['Content-Type: application/json', "$auth_key_name: $api_key"]);
+
+        //Set other required fields for request
+        $params['model'] = $this->model;
+        $params['messages'] = !empty($params['messages']) ?? [];
+        $params['max_completion_tokens'] = $this->defaultParams['max_tokens'];
+        $params['reasoning_effort'] = $params['reasoning_effort'] ?? $this->defaultParams['reasoning_effort'];
+        return json_encode($params);
     }
 }
